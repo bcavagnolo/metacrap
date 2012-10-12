3fc8aceef320db5b79bea59e7f52f6a6b7da2580
Bharath <bharathkumarguna@gmail.com>
2012-10-12 13:51:52 -0700

    Implementing Fuzzysearch

--- js/map.js
+++ js/map.js
@@ -10,11 +10,18 @@ Description: Javascript for Project 2
 	var map, geocoder, marker, infowindow, entity, tag, newTag;
 	var markersArray = [];
 	var psx;
+	var opt = {
+    nameSpace: "projectTwoTestPoints",
+    openKVURL: "http://riyadh.cusp.berkeley.edu/",
+    listID: 'point-store-list',
+  };
+
+  var infowindow = new google.maps.InfoWindow();
 
 
 	window.onload = function(){ 
 
-		psx = new PointStore("projectTwoTestPoints");
+		psx = new PointStore(opt);
 		console.log(psx);
 		var fix = new google.maps.LatLng(37.8717, -122.2728)
 		var options = {
@@ -53,51 +60,56 @@ Description: Javascript for Project 2
 		          placeMarker(e.latLng);
 		        });
 
-
-
 	}
 
 	function placeMarkers(entity){
 
-		clearMarkers();
+
 	
 		for (var i = 0; i < entity.length; i++) {
 
 			entity[i].glatlng = new google.maps.LatLng(entity[i].posn[0], entity[i].posn[1]);
-			placeMarker(entity[i]);
-			decorateMarker(entity[i]);
-			//location.push(new google.maps.LatLng(entity[i].posn[0], entity[i].posn[1]));
+			createMarker(map,entity[i])
+
+
+		};
+		console.log(entity);
+	}
+
+		function createMarker(map, point) {
+		    
+		    var contentString = '<div id="info">' +
+						'<h2>'+point.name+'</h2>';
+			contentString += '<ul id="taglist">'
+				for (m in point.tags)
+				{
+					contentString += '<li class="tag close'+m+'"><span class="tags">'+point.tags[m]+'</span><span class="close close'+m+'">x</span></li>';
+				}				
+			contentString += '</ul><div><label>Add tag:</label><input type="text" id="addtag"/><input type="submit" id="addTagButton"/></div></div>';
+
+		    var marker = new google.maps.Marker({
+		        position: point.glatlng,
+		        map: map,
+		        title: 'Add info to point',
+		        });
+		    point.Gmarker = marker;
+
+		    google.maps.event.addListener(marker, 'click', function() {
+		        infowindow.setContent(contentString); 
+		        infowindow.open(map,marker);
+		        });
 		};
 
 
 
-	}
-
-	function  decorateMarker(a) {
-			var name = a.name;
-	        var details = '<div id="info">' +
-						'<h2>'+name+'</h2>';
-			details += '<ul id="taglist">'
-				for (m in a.tags)
-				{
-					details += '<li class="tag close'+m+'"><span class="tags">'+a.tags[m]+'</span><span class="close close'+i+'">x</span></li>';
-				}				
-			details += '</ul><div><label>Add tag:</label><input type="text" id="addtag"/><input type="submit" id="addTagButton"/></div></div>';
-			 var infowindow = new google.maps.InfoWindow(
-		      { content: details,
-		      });
-
-
-      }
-
 	function placeMarker(a){
-          marker = new google.maps.Marker({
+          a.marker = new google.maps.Marker({
           position: a.glatlng,
           map: map,
           title: 'Add info to point'
         });
 		    google.maps.event.addListener(marker, 'click', function() {
-		    infowindow.open(map,marker);
+		    infowindow.open(map,a.marker);
 
 				$('#addTagButton').live("click", function(event){
 					var newTag = $("#addtag").val();
@@ -108,15 +120,6 @@ Description: Javascript for Project 2
 
 
  			 });
-      }
-
-	function clearMarkers() {
-		if (markersArray) {
-		    for (i in markersArray) {
-		      markersArray[i].setMap(null);
-		    }
-		    markersArray.length = 0;
-		  }
       }
 
 
